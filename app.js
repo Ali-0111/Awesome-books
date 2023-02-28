b1790236dddd59475aec44297d0a8fdaf8b94ece
Ali01 <alirahanet@gmail.com>
2023-02-28 14:58:56 +0430

    send it for r

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+
 // array to save each object
 let collection  = [];
-
 // constructor for object
 function Book(b_title,b_author) {
     this.title = b_title;
     this.author = b_author;
+}
+
+function saveLocal(arr,key) {
+    localStorage.setItem(key,JSON.stringify(arr));
 }
 
 function create () {
@@ -12,31 +16,50 @@ function create () {
     let title = document.querySelector('.title-input');
     let author = document.querySelector('.author-input'); 
     collection.push(new Book(title.value,author.value));
-    // remove the text from input box
-    title.value = '';
-    author.value = '';
     // result for function
-    return collection;
+    // return collection;
 }
 
-function remove() {
+function show() {
+    
+    let title = document.querySelector('.title-input');
+    let author = document.querySelector('.author-input'); 
     const section  = document.querySelector('.main-data');
     const div_show = document.createElement('div');
     div_show.classList = 'outputs';
     div_show.innerHTML = `
-    <h2 class="title-show">Title</h2>
-    <h2 class="author-show">Author</h2>
+    <h2 class="title-show">${title.value}</h2>
+    <p class="author-show">${author.value}</p>
     <button class="rmv-btn">Remove</button>
     `;
     section.append(div_show);
+    // remove the text from input box
+    title.value = '';
+    author.value = '';
 }
+
+function del () {
+    const btns = document.querySelectorAll('.rmv-btn');
+    console.log(btns);
+    const section = document.querySelector('section');
+    const div_show = document.querySelector('.div_show');
+    console.log("We are inside the function");
+    btns.forEach((btn)=>{
+        btn.addEventListener('click', () => {
+            section.removeChild(div_show);
+        });
+    });
+        
+};
 function main() {
     const addBtn =  document.querySelector('.add-btn');
     addBtn.addEventListener('click',() =>{
-        console.log(create());
-        remove();
+        create();
+        saveLocal(collection,"data");
+        show();
     });
+
+   
 }
-
 main();
-
+del ();
