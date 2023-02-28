570291767fca75e554ac39e4be7ba0f1844d054a
Ali01 <alirahanet@gmail.com>
2023-02-28 19:31:48 +0430

    40% done script

--- app.js
+++ app.js
@@ -1,65 +1,50 @@
 
-// array to save each object
-let collection  = [];
 // constructor for object
-function Book(b_title,b_author) {
-    this.title = b_title;
-    this.author = b_author;
-}
+class Book {
 
-function saveLocal(arr,key) {
-    localStorage.setItem(key,JSON.stringify(arr));
-}
+    constructor() {
+        this.collection  = [];
+        this.ul = document.querySelector('.lst');
+        this.title = document.querySelector('.title-input');
+        this.author = document.querySelector('.author-input'); 
+        this.addBtn =  document.querySelector('.add-btn');
+        this.fx;
+    }
 
-function create () {
-    // DOM  for inputs
-    let title = document.querySelector('.title-input');
-    let author = document.querySelector('.author-input'); 
-    collection.push(new Book(title.value,author.value));
-    // result for function
-    // return collection;
-}
+    saveLocal(arr,key) {
+        localStorage.setItem(key,JSON.stringify(arr));
+    }
 
-function show() {
-    
-    let title = document.querySelector('.title-input');
-    let author = document.querySelector('.author-input'); 
-    const section  = document.querySelector('.main-data');
-    const div_show = document.createElement('div');
-    div_show.classList = 'outputs';
-    div_show.innerHTML = `
-    <h2 class="title-show">${title.value}</h2>
-    <p class="author-show">${author.value}</p>
-    <button class="rmv-btn">Remove</button>
-    `;
-    section.append(div_show);
-    // remove the text from input box
-    title.value = '';
-    author.value = '';
-}
-
-function del () {
-    const btns = document.querySelectorAll('.rmv-btn');
-    console.log(btns);
-    const section = document.querySelector('section');
-    const div_show = document.querySelector('.div_show');
-    console.log("We are inside the function");
-    btns.forEach((btn)=>{
-        btn.addEventListener('click', () => {
-            section.removeChild(div_show);
+    createList(newBook) {
+        const li = document.createElement('li');
+        li.innerHTML = `
+        <h2 class="title-show">${newBook.title}</h2>
+        <p class="author-show">${newBook.author}</p>
+        <button class="rmv-btn">Remove</button>
+        `;
+        this.ul.appendChild(li);
+        this.fx = document.querySelectorAll('.rmv-btn');
+        // remove the text from input box
+    }
+    create () {
+        // DOM  for inputs
+        const newBook = {title: this.title.value, author: this.author.value }       
+         console.log(newBook, 'I created');
+        this.collection.push(newBook);
+        // return collection;
+       this.createList(newBook);
+    }
+   
+};
+const start = new Book();
+start.addBtn.addEventListener ('click', ()=> {
+    start.create();
+    start.fx.forEach((btn,i)=>{
+        btn.addEventListener('click', ()=>{
+            console.log(btn,i);
         });
     });
-        
-};
-function main() {
-    const addBtn =  document.querySelector('.add-btn');
-    addBtn.addEventListener('click',() =>{
-        create();
-        saveLocal(collection,"data");
-        show();
-    });
+});
 
-   
-}
-main();
-del ();
+
+
